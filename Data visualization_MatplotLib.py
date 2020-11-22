e979a98eadbaf85e9916236f8928c84bd092d972
kingshankha <kingshankha14@gmail.com>
2020-11-22 21:15:35 +0530

    vizualization issues fixed
    pycharm used
    
    missing data handling added

--- Data visualization_MatplotLib.py
+++ Data visualization_MatplotLib.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 df=pd.read_csv("Toyota.csv",index_col=0,na_values=["??","????"])
 df.dropna(axis=0,inplace=True)
 
@@ -18,11 +19,12 @@ df.dropna(axis=0,inplace=True)
 # Scaterplot to see the relationship between two variables
 # plt.figure(1)
 # =============================================================================
+plt.figure(1)
 plt.scatter(df["Age"],df["Price"],c='red')
-plt1.xlabel("Age")
-plt1.ylabel("Price")
-plt1.title("cars Age vs cars Price Data")
-
+plt.xlabel("Age")
+plt.ylabel("Price")
+plt.title("cars Age vs cars Price Data")
+plt.show()
 
 # =============================================================================
 # #### HYSTOGRAM ###########
@@ -39,14 +41,21 @@ plt1.title("cars Age vs cars Price Data")
 plt.figure(2)
 hist=plt.hist(df["KM"],color="cyan",edgecolor="blue",bins=5)
 plt.title("Histogram of kilometer variablem /freq dist")
-
+plt.show()
 
 # =============================================================================
 # BAR PLOT
 # Bar plot represents the frequency distribution 
 # of unique catagories of a catagories of a catagorical variable
 # =============================================================================
+#%%
+plt.figure(3)
+FuelType=df["FuelType"].value_counts().values
+index=np.arange(len(FuelType))
+plt.bar(index,FuelType,color=["blue","green","red"])
+plt.xticks(index,df["FuelType"].value_counts().index)
+plt.show()
 
-fuelType=("petrol","diesel","CNG")
-index=np.arrange(len("fuelType""))
-plt.bar()                
+
+
+
